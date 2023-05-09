7c7a94388d3b1ad945e08f581d20b233ba61ba0a
Alireza Ebadi Ghajari <alirezae@microsoft.com>
2023-05-08 17:49:10 -0700

    Add initial web controls

---
+++ CustomMapControl/WebControls/LoadMap.js
@@ -0,0 +1,7 @@
+﻿function GetMap() {
+    var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        center: new Microsoft.Maps.Location(51.50632, -0.12714),
+        zoom: 12,
+        mapTypeId: Microsoft.Maps.MapTypeId.aerial,
+    });
+}
