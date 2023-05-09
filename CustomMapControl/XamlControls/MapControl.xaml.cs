43256907d518e3890c633ee3071d264944c78535
Alireza Ebadi Ghajari <alirezae@microsoft.com>
2023-05-08 18:03:16 -0700

    Add Xaml controls

---
+++ CustomMapControl/XamlControls/MapControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Windows.Devices.Geolocation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace CustomMapControl.XamlControls
+{
+    public sealed partial class MapControl : UserControl
+    {
+        public static readonly DependencyProperty CenterProperty = DependencyProperty.Register(
+            "Items",
+            typeof(Geopoint),
+            typeof(MapControl),
+            new PropertyMetadata(null, null));
+
+        public MapControl()
+        {
+            InitializeComponent();
+        }
+
+        public Geopoint Center
+        {
+            get => (Geopoint)GetValue(CenterProperty);
+            set => SetValue(CenterProperty, value);
+        }
+    }
+}
